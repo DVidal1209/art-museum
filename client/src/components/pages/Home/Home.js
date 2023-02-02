29e933a0020edef0269489f3bb0ddaa9f097d538
Nick Prater <NikPratr@gmail.com>
2023-02-01 20:48:41 -0500

    styled homepage to feature showcases and updated html

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -6,7 +6,25 @@ export default function Home() {
   return (
     <>
     <div id='hero'>
+      <h1>Thought provoking text</h1>
     </div>
+
+    <section id='features'>
+      <div className='feature-showcase'>
+        <h2 className='feature-title'>Your Museum</h2>
+        <h4 className='feature-description'>Your place to express yourself creatively</h4>
+      </div>
+
+      <div className='feature-showcase'>
+        <h2 className='feature-title'>Your Exhibits</h2>
+        <h4 className='feature-description'>A themed section for your art</h4>
+      </div>
+
+      <div className='feature-showcase'>
+        <h2 className='feature-title'>The Plaza</h2>
+        <h4 className='feature-description'>Discover other artists</h4>
+      </div>
+    </section>
     </>
   )
 }
